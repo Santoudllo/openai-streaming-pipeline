20cb5b13b613e30cfa7a72e0a97e8db3da2decb3
Santoudllo <santou703@gmail.com>
2024-10-11 21:59:58 +0200

    spark

--- processing/spark_processing.py
+++ processing/spark_processing.py
@@ -9,16 +9,17 @@ def handle_client(client_socket):
             print("Connexion fermée par le client.")
             break
         try:
-            # Essayer de décoder les données JSON reçues
+            # décoder les données JSON reçues
+
             json_data = json.loads(data)
-            print(f"Données reçues : {json_data}")  # Afficher les données reçues
+            print(f"Données reçues : {json_data}")  # les données reçues
         except json.JSONDecodeError:
-            print("Erreur de décodage JSON : ", data)  # Gérer les erreurs de décodage
+            print("Erreur de décodage JSON : ", data)  # les erreurs de décodage
 
     client_socket.close()
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 1234))  # Utilisez le même port que le client
+server_socket.bind(('localhost', 1234))  
 server_socket.listen(5)
 print("En attente d'une connexion...")
 
