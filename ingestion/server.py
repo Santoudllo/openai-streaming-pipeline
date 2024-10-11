a6e10c6e4ec547971d2884c6a98f17d25bee139b
Santoudllo <santou703@gmail.com>
2024-10-11 21:58:14 +0200

    TPC /IP

--- ingestion/server.py
+++ ingestion/server.py
@@ -2,9 +2,11 @@ import socket
 import threading
 
 # Fonction pour gérer les connexions clients
+
 def handle_client(client_socket):
     while True:
         # Recevoir des données du client
+
         data = client_socket.recv(1024).decode()
         if not data:
             break
@@ -12,16 +14,20 @@ def handle_client(client_socket):
     client_socket.close()
 
 # Créer un socket TCP
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Lier le socket à l'adresse IP et au port
-server_socket.bind(('localhost', 1234))  # Le même port que dans client.py
+
+server_socket.bind(('localhost', 1234)) 
 
 # Écouter les connexions entrantes
+
 server_socket.listen(5)
 print("En attente d'une connexion...")
 
 # Accepter les connexions des clients
+
 while True:
     client_socket, address = server_socket.accept()
     print(f"Connexion établie avec {address}")
