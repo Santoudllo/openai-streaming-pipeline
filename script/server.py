74f36141d4a573b21a1c9bb02571c1288f095fde
Santoudllo <santou703@gmail.com>
2024-10-11 20:47:30 +0200

    scr server

---
+++ script/server.py
@@ -0,0 +1,23 @@
+import socket
+import time
+import json
+
+# Créer un socket TCP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Se connecter au serveur
+client_socket.connect(('localhost', 12345))
+
+# Simuler l'envoi de données
+for i in range(10):
+    data = {
+        'id': i,
+        'value': f'Donnée {i}',
+        'timestamp': time.time()
+    }
+    client_socket.sendall(json.dumps(data).encode())
+    print(f"Données envoyées : {data}")
+    time.sleep(1)  # Attendre 1 seconde entre les envois
+
+# Fermer la connexion
+client_socket.close()
